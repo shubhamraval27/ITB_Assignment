6f3ac80dcde09e7e7186c8233b04f7fa11335ed9
shubhamraval27 <ineedfood2704@gmail.com>
2025-02-25 16:34:38 +0530

    2203031080059

--- Assignment1/src/app/app.component.ts
+++ Assignment1/src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+// import { LoginComponent } from './auth-user/login/login.component';
+import { RegisterComponent } from './auth-user/register/register.component';
+// import { UserComponent } from './auth-user/user/user.component';
+// import { ForgotpasswordComponent } from './auth-user/forgotpassword/forgotpassword.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RegisterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
