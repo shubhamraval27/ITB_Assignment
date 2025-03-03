df70c3a1bb7da32851fb262fcaa6001d00e85d2c
shubhamraval27 <ineedfood2704@gmail.com>
2025-03-03 11:28:21 +0530

    2203031080059

--- Assignment1/src/app/app.component.ts
+++ Assignment1/src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-// import { LoginComponent } from './auth-user/login/login.component';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { LoginComponent } from './auth-user/login/login.component';
 import { RegisterComponent } from './auth-user/register/register.component';
-// import { UserComponent } from './auth-user/user/user.component';
-// import { ForgotpasswordComponent } from './auth-user/forgotpassword/forgotpassword.component';
+import { UserComponent } from './auth-user/user/user.component';
+import { ForgotpasswordComponent } from './auth-user/forgotpassword/forgotpassword.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RegisterComponent],
+  imports: [RouterOutlet,RouterLink,RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- Assignment1/src/app/app.routes.ts
+++ Assignment1/src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './auth-user/login/login.component';
+import { RegisterComponent } from './auth-user/register/register.component';
+import { ForgotpasswordComponent } from './auth-user/forgotpassword/forgotpassword.component';
+import { UserComponent } from './auth-user/user/user.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path : 'login',
+        component : LoginComponent
+
+    },
+    {
+        path : 'register',
+        component : RegisterComponent
+
+    },
+    {
+        path : 'forgotpassword',
+        component : ForgotpasswordComponent
+
+    },
+    {
+        path : 'user',
+        component : UserComponent
+
+    },
+
+];
